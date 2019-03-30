675f862197f557b20d612e16226781eb3a4a901a
luisNovaes <luismagnovaes@gmail.com>
2019-03-30 09:31:30 -0300

    startup do projeto

---
+++ src/main/java/com/magsoltec/controle/ControleInstApplication.java
@@ -0,0 +1,13 @@
+package com.magsoltec.controle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleInstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleInstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magsoltec/controle/ControleInstApplicationTests.java
@@ -0,0 +1,16 @@
+package com.magsoltec.controle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControleInstApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
